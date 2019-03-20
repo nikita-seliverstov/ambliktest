434468add076e005f02873ffd801ccb7cf469ea5
nikita <nikita.seliverstov@tptlive.ee>
2019-03-20 15:15:52 +0200

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import AmblikPagePreview from './preview-templates/ProductPagePreview'
 
+CMS.registerPreviewTemplate('Landing', AboutPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
